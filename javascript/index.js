e1bd05dd07c08a88a7f2750f2bfdb3b7e6767f68
akrem0-jr <abdulbasetadem@gmail.com>
2024-05-12 13:08:17 +0300

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const downloadButton = document.getElementById('downloadBtn');
+
+  downloadButton.addEventListener('click', function () {
+    // Add your download functionality here
+    alert('Download button clicked');
+  });
+});
